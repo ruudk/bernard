b6912f324cdd9d912eb55acc28eb68e93a52186e
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-05-28 08:20:35 +0200

    Add spec for JMS and Symfony Serializer

---
+++ spec/Bernard/Serializer/JMSSerializerSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace spec\Bernard\Serializer;
+
+use Bernard\Message\Envelope;
+use JMS\Serializer\SerializationContext;
+
+class JMSSerializerSpec extends \PhpSpec\ObjectBehavior
+{
+    /**
+     * @param JMS\Serializer\SerializerInterface $serializer
+     * @param Bernard\Message $message
+     */
+    function let($serializer)
+    {
+        $this->beConstructedWith($serializer);
+    }
+
+    function it_serializes_into_json_while_preserving_null($serializer, $message)
+    {
+        $envelope = new Envelope($message->getWrappedObject());
+        $context = SerializationContext::create()
+            ->setSerializeNull(true);
+
+        $serializer->serialize($envelope, 'json', $context)
+            ->shouldBeCalled()->willReturn('json encoded');
+
+        $this->serialize($envelope)->shouldReturn('json encoded');
+    }
+
+    function it_deserializes_json_into_an_envelope_and_return($serializer, $message)
+    {
+        $envelope = new Envelope($message->getWrappedObject());
+        $serializer->deserialize('json encoded', 'Bernard\Message\Envelope', 'json')
+            ->shouldBeCalled()->willReturn($envelope);
+
+        $this->deserialize('json encoded')->shouldReturn($envelope);
+    }
+}
---
+++ spec/Bernard/Serializer/SymfonySerializerSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace spec\Bernard\Serializer;
+
+use Bernard\Message\Envelope;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SymfonySerializerSpec extends ObjectBehavior
+{
+    /**
+     * @param Symfony\Component\Serializer\SerializerInterface $serializer
+     * @param Bernard\Message $message
+     */
+    function let($serializer)
+    {
+        $this->beConstructedWith($serializer);
+    }
+
+    function it_serializes_into_json($serializer, $message)
+    {
+        $envelope = new Envelope($message->getWrappedObject());
+
+        $serializer->serialize($envelope, 'json')
+            ->shouldBeCalled()->willReturn('json encoded');
+
+        $this->serialize($envelope)->shouldReturn('json encoded');
+    }
+
+    function it_deserializes_json_into_envelope($serializer, $message)
+    {
+        $envelope = new Envelope($message->getWrappedObject());
+
+        $serializer->deserialize('json encoded', 'Bernard\Message\Envelope', 'json')
+            ->shouldBeCalled()->willReturn($envelope);
+
+
+        $this->deserialize('json encoded')->shouldReturn($envelope);
+    }
+}
